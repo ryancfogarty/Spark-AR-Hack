ce9c2d129f85c3cd3176eb87a8cece7fa447d9cd
Ryan Fogarty <rfogar2@uic.edu>
2020-02-23 12:03:27 +0000

    Approximate instantaneous velocity for more natural flick

--- flick.js
+++ flick.js
@@ -5,45 +5,67 @@ const TouchGestures = require("TouchGestures")
 Diagnostics.log("Running flick.js")
 
 const hat = Scene.root.find("hat");
-
 const hatDepth = 3.5
 const hatStartPosition = hat.transform.position
 
-var startPanTime = null
-var yTranslation = null
+var touchFrames = []
 
-// todo: approximate instantaneous velocity instead of average velocity for more realistic flick
 function pixelsPerMs(translation, ms) {
-    return translation / ms
-}
-
-function timeSincePanStart() {
-    return Date.now() - startPanTime
+    return ms == 0 ? 0 : translation / ms
 }
 
 function resetHat() {
     hat.transform.position = hatStartPosition
 }
 
+function timeDiff(frames) {
+    if (frames.length == 0) return 0
+
+    const start = frames[frames.length - 1].timestamp;
+    const end = frames[0].timestamp;
+
+    return end - start;
+}
+
+function positionDiff(frames) {
+    if (frames.length == 0) return 0
+
+    const start = frames[frames.length - 1].yPosition;
+    const end = frames[0].yPosition;
+
+    return end - start;
+}
+
 TouchGestures.onPan(hat).subscribe(function(gesture) {
     const gesturePosition = Scene.unprojectWithDepth(gesture.location, hatDepth);
     hat.transform.position = gesturePosition
     
-    gesture.translation.y.monitor().subscribe((yt) => {
-        yTranslation = yt.newValue
+    gesturePosition.y.monitor().subscribe((yt) => {
+        var frame = {
+            yPosition: yt.newValue,
+            timestamp: Date.now()
+        }
+
+        touchFrames.splice(0, 0, frame)
+        if (touchFrames.length > 5) {
+            touchFrames.splice(5, touchFrames.length - 5)
+        }
     })
 
     gesture.state.monitor().subscribe((state) => {
         if (state.oldValue == "BEGAN") {
-            startPanTime = Date.now()
+            touchFrames = []
         } else if (state.newValue == "ENDED") {
-            if (yTranslation > 0) {
+            const timeDifference = timeDiff(touchFrames)
+            const positionDifference = positionDiff(touchFrames)
+
+            Diagnostics.log(pixelsPerMs(positionDifference, timeDifference))
+
+            if (positionDifference > 0) { // flicked down screen
                 Diagnostics.log("resetting hat position")
                 // `resetHat()` crashes due to "two or more patches connected multiple times"
                 // resetHat()
             }
-
-            Diagnostics.log(pixelsPerMs(yTranslation, timeSincePanStart()))
         }
     })
 })
